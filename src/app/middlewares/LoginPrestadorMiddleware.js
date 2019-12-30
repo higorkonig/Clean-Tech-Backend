f442b555f38f2a5353b792ff7d1e6acfb493d053
Higor Konig <higor.konig@gmail.com>
2019-12-30 19:35:32 -0300

    Middleware de verificar token, criado

---
+++ src/app/middlewares/LoginPrestadorMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import tokenConfig from '../../config/token';
+
+export default async (req, res, next) => {
+	const tokenHeader = req.headers.authorization;
+
+	if (!tokenHeader) {
+		res.status(401).json({ erro: 'Token não fornecido' });
+	}
+
+	const [, token] = tokenHeader.split(' ');
+
+	try {
+		const decoder = await promisify(jwt.verify)(token, tokenConfig.secret);
+
+		req.prestadorId = decoder.id;
+
+		return next();
+	} catch (err) {
+		return res
+			.status(401)
+			.json({ erro: 'Token fornecido é inválido ou já expirou' });
+	}
+};
--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,14 @@ import { Router } from 'express';
 import PrestadorController from './app/controllers/PrestadorController';
 import LoginPrestadorController from './app/controllers/LoginPrestadorController';
 
+
+//Middlewares
+import LoginPrestadorMiddleware from './app/middlewares/LoginPrestadorMiddleware';
 const routes = Router();
 
 routes.post('/prestador', PrestadorController.store);
 routes.post('/login', LoginPrestadorController.store);
+
+routes.use(LoginPrestadorMiddleware);
 
 export default routes;
